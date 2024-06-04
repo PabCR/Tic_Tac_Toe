d9900ab33dcd463d9322a6a2207cf90196837ae1
PabCR <palv2602@gmail.com>
2024-06-04 15:50:37 -0400

    Game functionality in command-line

--- lib/tic_tac_toe.ex
+++ lib/tic_tac_toe.ex
@@ -2,26 +2,41 @@ defmodule TicTacToe do
   @moduledoc """
   Documentation for `TicTacToe`.
   """
+  alias TicTacToe.Board
+
   def play(p1 \\ "P1", p2 \\ "P2") do
     board = TicTacToe.Board.create_board()
     IO.puts("Welcome #{p1} and #{p2} \n #{TicTacToe.Board.board_string(board)}")
     IO.puts("Player 1: #{p1} plays X and Player 2: #{p2} plays O")
-    play_game(board, p1, p2, 0)
+    play_game(board, p1, p2, 0, "X")
   end
 
-  def play_game(board, p1, p2, counter) do
-    case counter do
-      9 ->
+  def play_game(board, p1, p2, counter, symbol) do
+    case Board.check_win(board) do
+      true ->
+        IO.puts("Player #{symbol} wins")
         IO.puts("Game Over")
 
-      _ ->
-        IO.puts("Player 1: #{p1} plays X")
-        board = get_valid_move(board, "X")
-        IO.puts(TicTacToe.Board.board_string(board))
-        IO.puts("Player 2: #{p2} plays O")
-        board = get_valid_move(board, "O")
-        IO.puts(TicTacToe.Board.board_string(board))
-        play_game(board, p1, p2, counter + 2)
+      false ->
+        case counter do
+          9 ->
+            IO.puts("Game Over")
+
+          _ ->
+            case symbol do
+              "O" ->
+                IO.puts("Player 1: #{p1} plays X")
+                board = get_valid_move(board, "X")
+                IO.puts(Board.board_string(board))
+                play_game(board, p1, p2, counter + 1, "X")
+
+              "X" ->
+                IO.puts("Player 2: #{p2} plays O")
+                board = get_valid_move(board, "O")
+                IO.puts(Board.board_string(board))
+                play_game(board, p1, p2, counter + 1, "O")
+            end
+        end
     end
   end
 
@@ -39,6 +54,6 @@ defmodule TicTacToe do
   defp get_move(board, piece) do
     row = IO.gets("Enter row:") |> String.trim() |> String.to_integer()
     column = IO.gets("Enter column:") |> String.trim() |> String.to_integer()
-    TicTacToe.Board.edit_board(board, row, column, piece)
+    Board.edit_board(board, row, column, piece)
   end
 end
--- lib/tic_tac_toe/board.ex
+++ lib/tic_tac_toe/board.ex
@@ -25,4 +25,28 @@ defmodule TicTacToe.Board do
         {:error, "Invalid Position"}
     end
   end
+
+  def check_win(board) do
+    win_conditions = [
+      # Horizontal
+      [[0, 0], [0, 1], [0, 2]],
+      [[1, 0], [1, 1], [1, 2]],
+      [[2, 0], [2, 1], [2, 2]],
+      # Vertical
+      [[0, 0], [1, 0], [2, 0]],
+      [[0, 1], [1, 1], [2, 1]],
+      [[0, 2], [1, 2], [2, 2]],
+      # Diagonal
+      [[0, 0], [1, 1], [2, 2]],
+      [[0, 2], [1, 1], [2, 0]]
+    ]
+
+    Enum.any?(win_conditions, fn [a, b, c] ->
+      Enum.at(Enum.at(board, Enum.at(a, 0)), Enum.at(a, 1)) != "-" &&
+        Enum.at(Enum.at(board, Enum.at(a, 0)), Enum.at(a, 1)) ==
+          Enum.at(Enum.at(board, Enum.at(b, 0)), Enum.at(b, 1)) &&
+        Enum.at(Enum.at(board, Enum.at(a, 0)), Enum.at(a, 1)) ==
+          Enum.at(Enum.at(board, Enum.at(c, 0)), Enum.at(c, 1))
+    end)
+  end
 end
